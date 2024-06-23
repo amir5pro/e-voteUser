f467608b5d4c50e665d47f0cae3157e4b31dbfd8
amir miftah <amirmiftah555@gmail.com>
2024-06-23 16:36:22 +0300

    final2 commit

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const customFetch = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_BACKEND_URL,
 });
 
 export default customFetch;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:5100/api",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5100/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
